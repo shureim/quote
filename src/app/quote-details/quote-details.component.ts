787cafb84d6797ad7da5bce7d47e6c5633e41db6
tarick ali <tarickaliabdi@gmail.com>
2018-09-17 10:18:55 +0300

    added the qoute.details component.ts

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,6 +1,6 @@
-// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-// import { Quote } from '../quote';
-//
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+
 // @Component({
 //   selector: 'app-quote-details',
 //   templateUrl: './quote-details.component.html',
@@ -20,5 +20,5 @@
 //
 //   ngOnInit() {
 //   }
-//
-// }
+
+}
