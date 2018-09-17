ba2c7151947c80bf52064338a884a488721f34a6
tarick ali <tarickaliabdi@gmail.com>
2018-09-17 10:00:54 +0300

    initial app

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Quote } from '../quote';
-
-@Component({
-  selector: 'app-quote-details',
-  templateUrl: './quote-details.component.html',
-  styleUrls: ['./quote-details.component.css']
-})
-export class QuoteDetailsComponent implements OnInit {
-
-  @Input() quote: Quote;
-  @Output() isComplete = new EventEmitter<boolean>();
-
-  delete(complete: boolean) {
-    this.isComplete.emit(complete);
-  }
-
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// import { Quote } from '../quote';
+//
+// @Component({
+//   selector: 'app-quote-details',
+//   templateUrl: './quote-details.component.html',
+//   styleUrls: ['./quote-details.component.css']
+// })
+// export class QuoteDetailsComponent implements OnInit {
+//
+//   @Input() quote: Quote;
+//   @Output() isComplete = new EventEmitter<boolean>();
+//
+//   delete(complete: boolean) {
+//     this.isComplete.emit(complete);
+//   }
+//
+//
+//   constructor() { }
+//
+//   ngOnInit() {
+//   }
+//
+// }
