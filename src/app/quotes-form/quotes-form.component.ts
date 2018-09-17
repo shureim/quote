2569b1243455d8c23abe19fc6e2308e1f4bf3287
tarick ali <tarickaliabdi@gmail.com>
2018-09-17 10:03:48 +0300

    added the components

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,11 +1,11 @@
-// import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-// import {Quote} from '../quote'
-//
-// @Component({
-//   selector: 'app-quotes-form',
-//   templateUrl: './quotes-form.component.html',
-//   styleUrls: ['./quotes-form.component.css']
-// })
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {Quote} from '../quote'
+
+@Component({
+  selector: 'app-quotes-form',
+  templateUrl: './quotes-form.component.html',
+  styleUrls: ['./quotes-form.component.css']
+})
 // export class QuotesFormComponent implements OnInit {
 //   newQuote = new Quote(0, '', '', '', 0, 0, 0);
 //   @Output() addQuote = new EventEmitter<Quote>();
